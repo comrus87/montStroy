00860cdcc5f1491a3dbdc496574378065e895222
Andrew <nk-andrew1987@mail.ru>
2019-10-21 22:57:28 +0300

    Подклечен js

--- gulpfile.js
+++ gulpfile.js
@@ -41,6 +41,7 @@ gulp.task("server", function () {
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
   gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
+  gulp.watch("source/js/*.js", gulp.series("js", "refresh"));
 });
 
 gulp.task("refresh", function (done) {
@@ -81,6 +82,11 @@ gulp.task("html", function () {
     .pipe(gulp.dest("build"));
 });
 
+gulp.task("js", function () {
+  return gulp.src("source/js/*.js")
+    .pipe(gulp.dest("build/js"));
+});
+
 gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
@@ -97,5 +103,5 @@ gulp.task("clean", function () {
   return del("build");
 });
 
-gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
+gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html", "js"));
 gulp.task("start", gulp.series("build", "server"));
--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,30 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+const btnNav = document.querySelector('.page-header__nav-button');
+const navList = document.querySelector('.page-header__nav-list');
+const btnsServices =  document.querySelectorAll('.services__section-title');
+const servicesItems =  document.querySelectorAll('.services__section-item');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
+btnNav.addEventListener('click', function () {
+  navList.classList.toggle('page-header__nav-list--close');
 });
+
+function removeNoJs (block) {
+  if (block.classList.contains('no-js')) {
+    block.classList.remove('no-js');
+  }
+}
+
+removeNoJs(navList);
+
+
+btnsServices.forEach( function(button, i) {
+  button.addEventListener('click', function (evt) {
+    if (window.matchMedia('(max-width: 1023px)').matches) {
+      servicesItems[i].classList.toggle('services__section-item--active');
+    }
+
+  })
+});
+
+
