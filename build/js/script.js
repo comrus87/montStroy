7577e4d241fd7eb508f897d5da3a6c026d9813f9
Andrew <nk-andrew1987@mail.ru>
2019-10-22 15:33:33 +0300

    Осталось домучать слайдер

--- build/js/script.js
+++ build/js/script.js
@@ -1,9 +1,20 @@
 'use strict';
 
+const ERROR_NAME_MESSAGE = 'Имя должно начинаться с большой буквы, состоять как минимум из 2 и максимум из 24 букв';
+const ERROR_PHONE_MESSAGE = 'Введите корректный телефон';
+const ERROR_EMAIL_MESSAGE = 'Введите корректный E-mail';
+
 const btnNav = document.querySelector('.page-header__nav-button');
 const navList = document.querySelector('.page-header__nav-list');
 const btnsServices =  document.querySelectorAll('.services__section-title');
 const servicesItems =  document.querySelectorAll('.services__section-item');
+
+const name = document.querySelectorAll('.page-footer__form-input')[0];
+const phone = document.querySelectorAll('.page-footer__form-input')[1];
+const email = document.querySelectorAll('.page-footer__form-input')[2];
+const message = document.querySelectorAll('.page-footer__form-input')[3];
+const form = document.querySelector('.page-footer__form');
+
 
 btnNav.addEventListener('click', function () {
   navList.classList.toggle('page-header__nav-list--close');
@@ -17,14 +28,65 @@ function removeNoJs (block) {
 
 removeNoJs(navList);
 
-
 btnsServices.forEach( function(button, i) {
-  button.addEventListener('click', function (evt) {
+  button.addEventListener('click', function () {
     if (window.matchMedia('(max-width: 1023px)').matches) {
-      servicesItems[i].classList.toggle('services__section-item--active');
+      if (!(servicesItems[i].classList.contains('services__section-item--active'))) {
+        for (let j = 0; j < servicesItems.length; j++) {
+          servicesItems[j].classList.remove('services__section-item--active');
+        }
+        servicesItems[i].classList.add('services__section-item--active');
+      } else {
+        servicesItems[i].classList.remove('services__section-item--active');
+      }
     }
-
   })
 });
 
 
+const onNameValidate = function () {
+  const value = name.value.trim();
+  const reg = /^([А-Я]{1}[а-яё]{1,23}|[A-Z]{1}[a-z]{1,23})$/;
+
+  if (value.length > 0 && reg.test(value) == false) {
+    name.setCustomValidity(ERROR_NAME_MESSAGE);
+  } else {
+    name.setCustomValidity('');
+  }
+};
+
+name.addEventListener('input', onNameValidate);
+
+const phoneModalMask = IMask(phone, {mask: '+{7}(000)000-00-00'});
+
+
+const onPhoneValidate = function () {
+  const value = phone.value.trim();
+
+  if (value.length > 0 && value.length < 16) {
+    phone.setCustomValidity(ERROR_PHONE_MESSAGE);
+  } else {
+    phone.setCustomValidity('');
+  }
+};
+
+phone.addEventListener('input', onPhoneValidate);
+
+const onEmailValidate = function () {
+  const value = email.value.trim();
+  const reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+
+  if (reg.test(value) == false && value != '') {
+    console.log(value.length, value);
+    email.setCustomValidity(ERROR_EMAIL_MESSAGE);
+  } else {
+    email.setCustomValidity('');
+  }
+};
+
+email.addEventListener('input', onEmailValidate);
+
+form.addEventListener('submit', function (evt) {
+  evt.preventDefault();
+  form.reset();
+})
